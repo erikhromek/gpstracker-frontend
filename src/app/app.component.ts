a73a27dd8b6e39ce128605cdcbad6974cc0588e2
Erik Hromek <erikhromek96@gmail.com>
2024-04-18 01:43:07 -0300

    add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthUser } from './models/auth_user';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gpstracker-frontend';
+
+    /**
+   * An object representing the user for the login form
+   */
+    public user?: AuthUser;
+ 
+    constructor(private _authService: AuthService) { }
+   
+    ngOnInit() {
+      this.user = {
+        email: '',
+        password: ''
+      };
+    }
+   
+    login() {
+      this._authService.login(this.user);
+    }
+   
+    refreshToken() {
+      this._authService.refreshToken();
+    }
+   
+    logout() {
+      this._authService.logout();
+    }
+
 }
