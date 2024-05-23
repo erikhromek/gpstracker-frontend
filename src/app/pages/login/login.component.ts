d4d6fffe72494438332dd55ffc7ffd0fcff5d2da
nseverini <46693419+nseverini@users.noreply.github.com>
2024-05-23 12:53:41 -0300

    Fixed Angular Material theme issue

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,41 +4,53 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   providers: [AuthService],
-  imports: [FormsModule, CommonModule, HttpClientModule, MatCardModule, MatFormFieldModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   /**
    * An object representing the user for the login form
    */
   protected user: any;
- 
-  constructor(protected _authService: AuthService) { }
- 
+
+  constructor(protected _authService: AuthService) {}
+
   ngOnInit() {
     this.user = {
       email: '',
-      password: ''
+      password: '',
     };
   }
- 
+
   login() {
-    this._authService.login({'email': this.user.email, 'password': this.user.password});
+    this._authService.login({
+      email: this.user.email,
+      password: this.user.password,
+    });
   }
- 
+
   refreshToken() {
     this._authService.refreshToken();
   }
- 
+
   logout() {
     this._authService.logout();
   }
-
 }
