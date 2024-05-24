3eb93e61f96e70762f345e97b2d30c33ed3f70ef
Erik Hromek <erikhromek96@gmail.com>
2024-05-24 01:14:26 -0300

    Add reset password page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,5 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent) },
   { path: 'register', loadComponent: () => import('./pages/register/register.component').then((m) => m.RegisterComponent) },
+  { path: 'reset-password', loadComponent: () => import('./pages/reset-password/reset-password.component').then((m) => m.ResetPasswordComponent) },
 ];
---
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-reset-password',
+  standalone: true,
+  providers: [AuthService],
+  imports: [
+    FormsModule,
+    CommonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    RouterLink,
+  ],
+  templateUrl: './reset-password.component.html',
+  styleUrl: './reset-password.component.scss'
+})
+export class ResetPasswordComponent {
+  
+  protected user: any;
+
+  constructor(protected _authService: AuthService) {}
+
+  ngOnInit() {
+    this.user = {
+      email: '',
+    };
+  }
+
+  resetPassword() {
+  }
+
+}
