a6f83dce64709c7a02697533d68dbda45f514051
nseverini <46693419+nseverini@users.noreply.github.com>
2024-06-06 00:09:50 -0300

    Created alert service. Started to use angular wrapper library for the map. Added basic markers for alerts.

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,56 +1,76 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { AfterViewInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { MatListModule } from '@angular/material/list';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import {
+  Icon,
+  Marker,
+  circle,
+  icon,
+  latLng,
+  marker,
+  polygon,
+  tileLayer,
+} from 'leaflet';
+import { CommonModule } from '@angular/common';
+import { AlertService } from '../../services/alert.service';
+import { Alert } from '../../models/alert';
 
 @Component({
   selector: 'app-map',
   standalone: true,
-  imports: [MatListModule],
+  imports: [CommonModule, MatListModule, LeafletModule],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MapComponent implements AfterViewInit {
+export class MapComponent implements OnInit {
   apiUrl = `${environment.apiUrl}`;
-  private map!: L.Map;
-
-  private initMap(): void {
-    this.map = new L.Map('map', {
-      center: [-34.92053826324929, -57.95286763580453],
-      zoom: 14,
-    });
-
-    const tiles = L.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      {
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         minZoom: 3,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }
-    );
+      }),
+    ],
+    zoom: 14,
+    center: latLng(-34.92053826324929, -57.95286763580453),
+  };
+  layers: Marker[] = [];
+  alerts$!: Observable<Alert[]>;
 
-    tiles.addTo(this.map);
+  constructor(private alertService: AlertService) {}
+
+  ngOnInit(): void {
+    this.getAlerts();
   }
 
-  constructor(private httpClient: HttpClient) {}
-
-  private tryGet(): void {
-    this.httpClient
-    .get<any[]>(`${this.apiUrl}/alerts-summary/`)
-    .subscribe(
-      (data: any[]) => {
-        console.log("ok");
-      }
+  private getAlerts(): void {
+    this.alerts$ = this.alertService.getAlerts().pipe(
+      tap((alerts: Alert[]) => {
+        if (alerts) {
+          this.assignMarkers(alerts);
+        }
+      })
     );
   }
 
-  ngAfterViewInit(): void {
-    this.initMap();
-    this.tryGet();
+  private assignMarkers(alerts: Alert[]): void {
+    alerts.forEach((alert) => {
+      let alertMarker = marker([alert.latitude, alert.longitude], {
+        icon: icon({
+          ...Icon.Default.prototype.options,
+          iconUrl: 'assets/marker-icon.png',
+          iconRetinaUrl: 'assets/marker-icon-2x.png',
+          shadowUrl: 'assets/marker-shadow.png',
+        }),
+      });
+
+      alertMarker.bindPopup(`<div>${alert.id}</div>`);
+      this.layers.push(alertMarker);
+    });
   }
 }
