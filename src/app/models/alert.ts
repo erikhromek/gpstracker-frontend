c5af10d5352a2dd2cbd694c515c2459d46ceaf3b
nseverini <46693419+nseverini@users.noreply.github.com>
2024-06-06 00:07:12 -0300

    Fixed and improved auth interceptor. Removed blank spaces.

--- src/app/models/alert.ts
+++ src/app/models/alert.ts
@@ -1,17 +1,15 @@
 export interface Alert {
-    id: number;
-    datetime: Date;
-    datetimeAttended?: Date;
-    datetimeClosed?: Date;
-    beneficiaryId: number;
-    telephone: string;
-    latitude: number;
-    longitude: number;
-    state: string;
-    operatorId?: number;
-    observations?: string;
-    typeId?: number;
-    messageSid: string;
-
-  }
-  
+  id: number;
+  datetime: Date;
+  datetimeAttended?: Date;
+  datetimeClosed?: Date;
+  beneficiaryId: number;
+  telephone: string;
+  latitude: number;
+  longitude: number;
+  state: string;
+  operatorId?: number;
+  observations?: string;
+  typeId?: number;
+  messageSid: string;
+}
